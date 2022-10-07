511a3715cd951e106e93ec4272abab3338368722
Juhdev2022 <98654148+Juhdev2022@users.noreply.github.com>
2022-10-07 07:31:32 -0300

    Delete ex011.js

--- 1Hora do dia/ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 16
-console.log(`Você tem ${idade} anos.`) 
-if(idade <16) {
-    console.log('Não vota')
-} else if(idade < 18 || idade > 65){
-        console.log('Voto opcional')
-    } else {
-        console.log('Voto obrigatório')
-    }
