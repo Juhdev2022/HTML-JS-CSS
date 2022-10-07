0dab2a33da977e0f7a3e287d7683d3f40ac66a2e
Juhdev2022 <98654148+Juhdev2022@users.noreply.github.com>
2022-10-07 07:34:10 -0300

    Delete ex009.js

--- 4Multas_limite de 60km/ex009.js
+++
@@ -1,7 +0,0 @@
-var país = 'Brasil'
-console.log(`Vivendo em ${país}`)
-if (país == 'Brasil'){
-    console.log('Você é Brasileiro!')
-} else{ // else = "senão"- condição composta pq tem 2 condicionais
-    console.log('Você é Estrangeiro!')
-}
